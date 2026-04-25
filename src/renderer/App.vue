c04ef9fd12e57e8fe7b4ad8c037f1785f4003ed6
鲁迅 <1501110991@qq.com>
2026-04-25 14:59:34 +0800

    feat: 首页样式；

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,48 +1,67 @@
 <!-- 文件：/src/renderer/App.vue -->
 
 <template>
-  <div>
-    <h1>Todo List</h1>
+  <div class="todo-container">
+    <div class="todo-header">
+      <h1>待办事项</h1>
+    </div>
 
-    <input v-model="input" @keyup.enter="addTodo" />
-    <button @click="addTodo">添加</button>
+    <div class="todo-input-group">
+      <input
+        v-model="input"
+        @keyup.enter="addTodo"
+        class="todo-input"
+        placeholder="输入待办事项..."
+      />
+      <button @click="addTodo" class="btn btn-primary">添加</button>
+    </div>
 
-    <ul>
-      <li v-for="item in list" :key="item.id">
-        {{ item.text }}
-        <button @click="remove(item.id)">删除</button>
+    <ul v-if="list.length > 0" class="todo-list">
+      <li v-for="item in list" :key="item.id" class="todo-item">
+        <span class="todo-item-text">{{ item.text }}</span>
+        <button @click="remove(item.id)" class="btn btn-danger">删除</button>
       </li>
     </ul>
+
+    <div v-else class="todo-empty">
+      <div class="todo-empty-icon">📝</div>
+      <div class="todo-empty-text">暂无待办事项</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, toRaw, watch } from 'vue'
+import { onMounted, ref, toRaw, watch } from "vue";
+import "@/assets/styles/app.css";
 
-const input = ref('')
-const list = ref<any[]>([])
+const input = ref("");
+const list = ref<any[]>([]);
 
 const addTodo = () => {
-  if (!input.value) return
+  if (!input.value) return;
   list.value.push({
     id: Date.now(),
-    text: input.value
-  })
-  input.value = ''
-}
+    text: input.value,
+  });
+  input.value = "";
+};
 
 const remove = (id: number) => {
-  list.value = list.value.filter(i => i.id !== id)
-}
+  list.value = list.value.filter((i) => i.id !== id);
+};
 
 // 组件挂载时从主进程获取已保存的待办事项列表
 onMounted(async () => {
-  list.value = await window.api.invoke('todo:get')
-})
+  list.value = await window.api.invoke("todo:get");
+});
 
 // 深度监听列表变化,当列表内容改变时自动同步到主进程
-watch(list, async (val) => {
-  // 使用 toRaw 将 Vue 响应式对象转换为普通对象,避免序列化错误
-  await window.api.invoke('todo:set', toRaw(val))
-}, { deep: true })
+watch(
+  list,
+  async (val) => {
+    // 使用 toRaw 将 Vue 响应式对象转换为普通对象,避免序列化错误
+    await window.api.invoke("todo:set", JSON.parse(JSON.stringify(val)));
+  },
+  { deep: true },
+);
 </script>
